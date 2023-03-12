74f0c66b2e1fda727b3fb742763fe1bd1b4b8018
Shubham Yadav <108620698+shubhamgyd@users.noreply.github.com>
2023-03-12 12:29:47 +0000

    made a first page

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'Salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'Salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'Salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'Salary': '$120,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World</p>"
+  return render_template('home.html',
+                        jobs = JOBS, company_name="Shubham")
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   print("I am inside the if statement.")
