8908b8fa0bb8b76dbd519a2132dd3e94ed1aa09f
Shubham Yadav <108620698+shubhamgyd@users.noreply.github.com>
2023-03-13 01:52:08 +0000

    made changes to db

--- database.py
+++ database.py
@@ -1,12 +1,47 @@
+import os
+import sqlalchemy
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://r02sjowi8fmff2uxmvqc:pscale_pw_xR8bcA9CMPUOnKesvFJAJJ83RcRWSWDn1wjUrLwtETV@ap-south.connect.psdb.cloud/careers_thadomal?charset=utf8mb4"
+# my_secret = os.environ['DB_CONNECTION_STRING']
+
+print(sqlalchemy.__version__)
+# my_secret = os.environ['DB_CONNECTION_KEY']
+
+db_connection_string= os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem",
                        }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print(result.all())
+# with engine.connect() as conn:
+#   result = conn.execute(text("select * from jobs"))
+
+#   '''
+#   print("type(result):", type(result))
+#   result_all = result.all()
+#   print("type(result.all())", type(result_all))
+#   first_result = result_all[0]
+#   print("type(first_result):",type(first_result))
+
+#   # Here we have converted alchemy row which is of type row to dictionary.
+#   first_result_dict = dict(result_all[0])
+#   print("type(first_result_dict):", type(first_result_dict))
+#   print(first_result_dict)
+#   '''
+  
+#   result_dicts = []
+
+#   for row in result.all():
+#     result_dicts.append(row._mapping)
+#     print(result_dicts)
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._mapping)
+    return jobs
+      
+  
--- main.py
+++ main.py
@@ -1,44 +1,23 @@
 from flask import Flask, render_template, jsonify
-
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'Salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'Salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-    'Salary': 'Rs. 12,00,000'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'Salary': '$120,000'
-  }
-]
+
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',
-                        jobs = JOBS, company_name="Thadomal")
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name="Thadomal")
+
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
 
 if __name__ == "__main__":
   print("I am inside the if statement.")
-  app.run(host='0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True)
